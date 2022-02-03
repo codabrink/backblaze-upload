981e0757205ef99adf19063372d53ee69bca5dc3
Dakota Brink <git@kota.is>
2022-02-02 21:02:48 -0500

    2 spaces

--- src/main.rs
+++ src/main.rs
@@ -17,77 +17,77 @@ use std::{ffi::OsStr, io::Cursor};
 use rodio::{source::Source, Decoder, OutputStream};
 
 fn main() {
-    let config = Config::load().unwrap();
-    let _ = std::fs::create_dir_all(&config.upload_dir);
+  let config = Config::load().unwrap();
+  let _ = std::fs::create_dir_all(&config.upload_dir);
 
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
-    let mut watcher = watcher(tx, Duration::from_millis(200)).unwrap();
-    watcher
-        .watch(&config.upload_dir, RecursiveMode::Recursive)
-        .unwrap();
-
-    let creds = Credentials::new(
-        Some(&config.access_key),
-        Some(&config.secret_key),
-        None,
-        None,
-        None,
-    )
+  // Create a channel to receive the events.
+  let (tx, rx) = channel();
+  let mut watcher = watcher(tx, Duration::from_millis(200)).unwrap();
+  watcher
+    .watch(&config.upload_dir, RecursiveMode::Recursive)
     .unwrap();
 
-    let region = Region::Custom {
-        region: config.region,
-        endpoint: config.endpoint,
-    };
-    let bucket = Bucket::new(&config.bucket, region, creds).unwrap();
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+  let creds = Credentials::new(
+    Some(&config.access_key),
+    Some(&config.secret_key),
+    None,
+    None,
+    None,
+  )
+  .unwrap();
 
-    let bell = include_bytes!("bird.ogg");
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+  let region = Region::Custom {
+    region: config.region,
+    endpoint: config.endpoint,
+  };
+  let bucket = Bucket::new(&config.bucket, region, creds).unwrap();
+  let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
 
-    loop {
-        match rx.recv() {
-            Ok(DebouncedEvent::Create(p)) if p.is_file() => {
-                println!("File detected: {:?}", p);
-                let bytes = std::fs::read(&p).unwrap();
+  let bell = include_bytes!("bird.ogg");
+  let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-                let file_name = p.file_name().unwrap();
-                let file_extension = p.extension().unwrap_or(OsStr::new("")).to_str().unwrap();
-                let obfuscated_file = format!("{}.{}", rand_string(5), file_extension);
+  loop {
+    match rx.recv() {
+      Ok(DebouncedEvent::Create(p)) if p.is_file() => {
+        println!("File detected: {:?}", p);
+        let bytes = std::fs::read(&p).unwrap();
 
-                let _ = match mime_guess::from_ext(file_extension).first() {
-                    Some(guess) => bucket.put_object_with_content_type_blocking(
-                        &obfuscated_file,
-                        &bytes,
-                        &guess.to_string(),
-                    ),
-                    _ => bucket.put_object_blocking(&file_name.to_str().unwrap(), &bytes),
-                };
+        let file_name = p.file_name().unwrap();
+        let file_extension = p.extension().unwrap_or(OsStr::new("")).to_str().unwrap();
+        let obfuscated_file = format!("{}.{}", rand_string(5), file_extension);
 
-                let _ = ctx.set_contents(format!("{}/{}", &config.base_url, &obfuscated_file));
+        let _ = match mime_guess::from_ext(file_extension).first() {
+          Some(guess) => bucket.put_object_with_content_type_blocking(
+            &obfuscated_file,
+            &bytes,
+            &guess.to_string(),
+          ),
+          _ => bucket.put_object_blocking(&file_name.to_str().unwrap(), &bytes),
+        };
 
-                println!("{:?} uploaded", file_name);
-                if config.delete_on_upload {
-                    let _ = std::fs::remove_file(p);
-                }
+        let _ = ctx.set_contents(format!("{}/{}", &config.base_url, &obfuscated_file));
 
-                if config.upload_sound {
-                    let reader = Cursor::new(bell);
-                    let source = Decoder::new(reader).unwrap();
-                    let _ = &stream_handle.play_raw(source.convert_samples());
-                }
-            }
-            Err(e) => println!("watch error: {:?}", e),
-            _ => {}
+        println!("{:?} uploaded", file_name);
+        if config.delete_on_upload {
+          let _ = std::fs::remove_file(p);
         }
+
+        if config.upload_sound {
+          let reader = Cursor::new(bell);
+          let source = Decoder::new(reader).unwrap();
+          let _ = &stream_handle.play_raw(source.convert_samples());
+        }
+      }
+      Err(e) => println!("watch error: {:?}", e),
+      _ => {}
     }
+  }
 }
 
 fn rand_string(len: usize) -> String {
-    thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(len)
-        .map(char::from)
-        .collect()
+  thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(len)
+    .map(char::from)
+    .collect()
 }
